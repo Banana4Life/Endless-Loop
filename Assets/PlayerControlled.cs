1e766930e4246fa8ca49a310c2868688f8a05b05
Anselm Brehme <development.anselm@breh.me>
2022-10-01 12:29:56 +0200

    grid movement

--- Assets/PlayerControlled.cs
+++ Assets/PlayerControlled.cs
@@ -1,10 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerControlled : MonoBehaviour
 {
     public float speed = 5;
+
+    public float maxMoveCooldown = 1;
+
+    private float _moveCooldown;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,21 @@ public class PlayerControlled : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        transform.position += movement * Time.deltaTime * speed;
+        if (_moveCooldown > 0)
+        {
+            _moveCooldown -= Time.deltaTime;
+        }
+
+        var hMove = Input.GetAxis("Horizontal");
+        var vMove = Input.GetAxis("Vertical");
+        if (Math.Abs(hMove) + Math.Abs(vMove) > 0)
+        {
+            if (_moveCooldown <= 0)
+            {
+                
+                _moveCooldown = maxMoveCooldown;
+                transform.position += new Vector3(Math.Sign(hMove), 0, Math.Sign(vMove)) * speed;
+            }
+        }
     }
 }
--- Assets/Scripts/CapsuleDissolver.cs
+++ Assets/Scripts/CapsuleDissolver.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 
 public class CapsuleDissolver : MonoBehaviour
 {
-    private MeshFilter filter;
-    private MeshRenderer renderer;
-    private float min;
+    private MeshFilter _filter;
+    private MeshRenderer _renderer;
+    private float _min;
 
-    private float max;
+    private float _max;
 
-    private float cutoff;
+    private float _cutoff;
 
     public float dissolveSpeed;
 
@@ -20,12 +20,12 @@ public class CapsuleDissolver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        filter = GetComponent<MeshFilter>();
-        renderer = GetComponent<MeshRenderer>();
-        min = filter.sharedMesh.vertices.ToList().Min(v => v.y) - 1;
-        max = filter.sharedMesh.vertices.ToList().Max(v => v.y) + 1;
-        cutoff = max;
-        renderer.sharedMaterial.SetFloat("_CutOffHeight", cutoff);
+        _filter = GetComponent<MeshFilter>();
+        _renderer = GetComponent<MeshRenderer>();
+        _min = _filter.sharedMesh.vertices.ToList().Min(v => v.y) - 1;
+        _max = _filter.sharedMesh.vertices.ToList().Max(v => v.y) + 1;
+        _cutoff = _max;
+        _renderer.sharedMaterial.SetFloat("_CutOffHeight", _cutoff);
     }
 
     // Update is called once per frame
@@ -33,13 +33,13 @@ public class CapsuleDissolver : MonoBehaviour
     {
         if (dissolveSpeed != 0)
         {
-            cutoff -= Time.deltaTime * dissolveSpeed;
-            renderer.sharedMaterial.SetFloat("_CutOffHeight", cutoff);
-            if (cutoff < min)
+            _cutoff -= Time.deltaTime * dissolveSpeed;
+            _renderer.sharedMaterial.SetFloat("_CutOffHeight", _cutoff);
+            if (_cutoff < _min)
             {
                 dissolveSpeed = 0;
             }
-            else if (cutoff > max)
+            else if (_cutoff > _max)
             {
                 dissolveSpeed = 0;
             }
@@ -50,14 +50,14 @@ public class CapsuleDissolver : MonoBehaviour
     {
         if (dissolveSpeed == 0)
         {
-            cutoff = max;
+            _cutoff = _max;
             dissolveSpeed = speed;
         }
     }
 
     public void StartUnDissolve(float speed)
     {
-        cutoff = min;
+        _cutoff = _min;
         dissolveSpeed = -speed;
     }
 }
