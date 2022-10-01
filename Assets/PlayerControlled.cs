3bf92210158e4d8c3af99acb9cb2bbf5130f8d78
Anselm Brehme <development.anselm@breh.me>
2022-10-01 13:27:33 +0200

    alternative movement

--- Assets/PlayerControlled.cs
+++ Assets/PlayerControlled.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 
 public class PlayerControlled : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 10f;
+    public float gridSpeed = 2.5f;
 
     public float maxMoveCooldown = 1;
 
+    public bool gridMove;
+
     private float _moveCooldown;
+
+    private Rigidbody _rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,16 +28,31 @@ public class PlayerControlled : MonoBehaviour
             _moveCooldown -= Time.deltaTime;
         }
 
-        var hMove = Input.GetAxis("Horizontal");
-        var vMove = Input.GetAxis("Vertical");
-        if (Math.Abs(hMove) + Math.Abs(vMove) > 0)
+        var hMove = Input.GetAxisRaw("Horizontal");
+        var vMove = Input.GetAxisRaw("Vertical");
+
+        if (gridMove)
         {
-            if (_moveCooldown <= 0)
+            if (Math.Abs(hMove) + Math.Abs(vMove) > 0)
             {
-                
-                _moveCooldown = maxMoveCooldown;
-                transform.position += new Vector3(Math.Sign(hMove), 0, Math.Sign(vMove)) * speed;
+                if (_moveCooldown <= 0)
+                {
+                    _moveCooldown = maxMoveCooldown;
+                    transform.position += new Vector3(Math.Sign(hMove), 0, Math.Sign(vMove)) * gridSpeed;
+                }
             }
+        }
+        else
+        {
+            if (Math.Abs(hMove) + Math.Abs(vMove) > 0)
+            {
+                _rigidbody.AddForce(new Vector3(hMove, 0, vMove).normalized * speed);    
+            }
+            else
+            {
+                _rigidbody.AddForce(new Vector3(hMove, 0, vMove).normalized * speed);
+            }
+            
         }
     }
 }
